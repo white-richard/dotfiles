b806412cc38bcba576251de5e69c7a72f77a8ff8
white-richard <rich.white285@yahoo.com>
2026-03-14 15:26:47 -0700

    added ruff config! very strict rules to start

--- install.fish
+++ install.fish
@@ -12,7 +12,7 @@ set -l remote_cmd "cd ~/.dotfiles && git pull && fish install.fish"
 # --- Config ---
 
 # Configs to be symlinked
-set APPS fish nvim tmux zed git
+set APPS fish nvim tmux zed git ruff
 
 # Machines to distribute to in .env as: SSH_MACHINES="machine1 machine2"
 if test -f .env
