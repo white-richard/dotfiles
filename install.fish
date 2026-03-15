1d2a36f8398d5e5f7cbdea375c395af9a5bdcd6d
white-richard <rich.white285@yahoo.com>
2026-03-14 21:01:23 -0700

    added ghostty config

--- install.fish
+++ install.fish
@@ -12,7 +12,7 @@ set -l remote_cmd "cd ~/.dotfiles && git pull && fish install.fish"
 # --- Config ---
 
 # Configs to be symlinked
-set APPS fish nvim tmux zed git ruff
+set APPS fish nvim tmux zed git ruff ghostty
 
 # Machines to distribute to in .env as: SSH_MACHINES="machine1 machine2"
 if test -f .env
