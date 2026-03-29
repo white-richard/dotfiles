931f77ffd68452be80efd68e0a10368c8562e48e
white-richard <rich.white285@yahoo.com>
2026-03-28 17:15:38 -0700

    update ghostty config; add slurm job command

---
+++ fish/functions/,sjob.fish
@@ -0,0 +1,41 @@
+function ,sjob
+    # Infer from current directory
+    set project_dir (pwd)
+    set job_name (basename $project_dir)
+
+    # Defaults with option overrides
+    set container_image "pytorch/pytorch:2.11.0-cuda12.8-cudnn9-devel"
+    set nodes 1
+    set ntasks 1
+    set gpus 1
+
+    # Parse args: slurm-submit --gpus 1 --time 04:00:00 --image myimage:tag
+    argparse \
+        'i/image=' \
+        'n/nodes=' \
+        't/ntasks=' \
+        'g/gpus=' \
+        -- $argv
+    or return 1
+
+    if set -q _flag_image;  set container_image $_flag_image;  end
+    if set -q _flag_nodes;  set nodes $_flag_nodes;            end
+    if set -q _flag_ntasks; set ntasks $_flag_ntasks;          end
+    if set -q _flag_gpus;   set gpus $_flag_gpus;              end
+
+    echo "Submitting job..."
+    echo "  Name:    $job_name"
+    echo "  Dir:     $project_dir"
+    echo "  Image:   $container_image"
+    echo "  Nodes:   $nodes  Tasks: $ntasks  GPUs: $gpus"
+    echo ""
+
+    env \
+        JOB_NAME=$job_name \
+        PROJECT_DIR=$project_dir \
+        CONTAINER_IMAGE=$container_image \
+        NODES=$nodes \
+        NTASKS=$ntasks \
+        GPUS=$gpus \
+        sbatch ~/.slurm/job.sh
+end
