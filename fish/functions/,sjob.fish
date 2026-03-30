05130cdbe4dc3b93072561d259f0ef7fd0639578
white-richard <rich.white285@yahoo.com>
2026-03-29 18:52:24 -0700

    update slurm scripts

--- fish/functions/,sjob.fish
+++ fish/functions/,sjob.fish
@@ -2,34 +2,40 @@ function ,sjob
     # Infer from current directory
     set project_dir (pwd)
     set job_name (basename $project_dir)
-
     # Defaults with option overrides
     set container_image (cat ~/.slurm/default_image)
     set nodes 1
     set ntasks 1
     set gpus 1
-
+    set nodelist ""
     argparse \
         'i/image=' \
         'n/nodes=' \
         't/ntasks=' \
         'g/gpus=' \
+        'w/worker=' \
         -- $argv
     or return 1
-
     if set -q _flag_image;  set container_image $_flag_image;  end
     if set -q _flag_nodes;  set nodes $_flag_nodes;            end
     if set -q _flag_ntasks; set ntasks $_flag_ntasks;          end
     if set -q _flag_gpus;   set gpus $_flag_gpus;              end
-
+    if set -q _flag_worker; set nodelist $_flag_worker;        end
     echo "Submitting job..."
     echo "  Name:    $job_name"
     echo "  Dir:     $project_dir"
     echo "  Image:   $container_image"
     echo "  Nodes:   $nodes  Tasks: $ntasks  GPUs: $gpus"
+    if test -n "$nodelist"
+        echo "  Worker:  $nodelist"
+    end
     echo ""
-
     docker cp ~/.slurm/job.sh slurmctld:/data/job.sh
+
+    set nodelist_flag
+    if test -n "$nodelist"
+        set nodelist_flag --nodelist=$nodelist
+    end
 
     docker exec \
         -e JOB_NAME=$job_name \
@@ -44,5 +50,6 @@ function ,sjob
             --ntasks=$ntasks \
             --partition=gpu \
             --gres=gpu:$gpus \
+            $nodelist_flag \
             /data/job.sh
 end
--- fish/functions/,slog.fish
+++ fish/functions/,slog.fish
@@ -1,5 +1,23 @@
 function ,slog
     set job_name (basename (pwd))
-    set pattern "/data/$job_name""_*.out"
-    docker exec slurmctld bash -c "tail -f -n +1 \$(ls -t $pattern | head -1)"
+    argparse \
+        'w/worker=' \
+        -- $argv
+    or return 1
+
+    if set -q _flag_worker
+        # Find the most recent job ID for this job name that ran on the specified node
+        set job_id (docker exec slurmctld bash -c \
+            "sacct -n -X --name=$job_name --nodelist=$_flag_worker --format=JobID --state=ANY \
+             | tail -1 | tr -d ' '")
+        if test -z "$job_id"
+            echo "No jobs found for '$job_name' on worker '$_flag_worker'"
+            return 1
+        end
+        echo "Tailing job $job_id (ran on $_flag_worker)..."
+        docker exec slurmctld bash -c "tail -f -n +1 /data/{$job_name}_{$job_id}.out"
+    else
+        set pattern "/data/$job_name""_*.out"
+        docker exec slurmctld bash -c "tail -f -n +1 \$(ls -t $pattern | head -1)"
+    end
 end
