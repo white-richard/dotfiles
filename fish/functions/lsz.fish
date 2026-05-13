6ad5bf13e122ec116f5855b6d2d0adfbe0281764
white-richard <98299003+white-richard@users.noreply.github.com>
2026-05-13 01:28:54 -0700

    update lsz to have a sudo flag

--- fish/functions/lsz.fish
+++ fish/functions/lsz.fish
@@ -1,3 +1,11 @@
 function lsz --description 'Print file sizes of files and dirs in current directory'
-    sudo du -sch (ls -A) | sort -h
+    # Parse arguments for -s or --sudo
+    argparse 's/sudo' -- $argv
+    or return 1
+
+    if set -ql _flag_sudo
+        sudo du -sch (ls -A) | sort -h
+    else
+        du -sch (ls -A) | sort -h
+    end
 end
